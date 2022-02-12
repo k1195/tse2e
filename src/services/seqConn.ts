4420d5adb303135b3a20fce234b4a56b4fc7dbd8
k1195 <kgalgat117@gmail.com>
2022-02-12 15:20:17 +0530

    yes

---
+++ src/services/seqConn.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+const sequelize = new Sequelize(process.env.CONNECTIONQUERY || '')
+
+try {
+    sequelize.authenticate()
+    console.log("Connection established to PG")
+} catch (error) {
+    console.log("Connection failed to PG")
+}
+
